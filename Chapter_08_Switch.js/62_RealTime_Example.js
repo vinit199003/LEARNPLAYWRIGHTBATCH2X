98d159f5f312058f5cc90d88ff24607125531e92
Vinit Bhanushali <vinit.bhanushali@jiostar.com>
2026-07-02 20:23:50 +0530

    LatestIfElsewithSwitchcase

---
+++ Chapter_08_Switch.js/62_RealTime_Example.js
@@ -0,0 +1,8 @@
+//API validation and switch case example with error code.
+
+let statusCode = 200;
+switch ( statusCode ) {
+    case 200:
+        console.log( "Error: Invalid request." );
+        break;
+}
